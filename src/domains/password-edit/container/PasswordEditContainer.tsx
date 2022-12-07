8fe304e70951df44b665d64f9147a7fae1792f4a
leejiho9898 <cctv1998@naver.com>
2022-12-07 23:21:26 +0900

    refactor: 소셜타입 방어 로직 분리

--- src/domains/password-edit/container/PasswordEditContainer.tsx
+++ src/domains/password-edit/container/PasswordEditContainer.tsx
@@ -1,18 +1,10 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import Path from "routes/Path";
-import { userSelector } from "stores/user";
 import { PasswordEditTemplate } from "../components";
 import PasswordEditForm from "../components/PasswordEditForm";
 import PasswordEditHead from "../components/PasswordEditHead";
+import useCheckSocialType from "../hooks/useCheckSocialType";
 
 function PasswordEditContainer() {
-  const { socialType } = useSelector(userSelector);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (socialType === "google") navigate(Path.MyPage);
-  }, [navigate, socialType]);
+  useCheckSocialType();
 
   return (
     <PasswordEditTemplate>
