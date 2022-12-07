608095164deb4509a1cb8026896f999af2b5919c
leejiho9898 <cctv1998@naver.com>
2022-12-07 23:18:57 +0900

    feat: 비밀번호 변경 socialType 안맞는 유저 방어 로직

--- src/domains/password-edit/container/PasswordEditContainer.tsx
+++ src/domains/password-edit/container/PasswordEditContainer.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Path from "routes/Path";
+import { userSelector } from "stores/user";
 import { PasswordEditTemplate } from "../components";
 import PasswordEditForm from "../components/PasswordEditForm";
 import PasswordEditHead from "../components/PasswordEditHead";
 
 function PasswordEditContainer() {
+  const { socialType } = useSelector(userSelector);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (socialType === "google") navigate(Path.MyPage);
+  }, [navigate, socialType]);
+
   return (
     <PasswordEditTemplate>
       <PasswordEditHead headText="비밀번호 변경" />
