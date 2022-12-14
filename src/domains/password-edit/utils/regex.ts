a51df554f44431647353ec49787d11d37b1f7b29
leejiho9898 <cctv1998@naver.com>
2022-12-15 02:34:26 +0900

    refactor: 비밀번호 정규식 리팩토링

--- src/domains/password-edit/utils/regex.ts
+++ src/domains/password-edit/utils/regex.ts
@@ -1,9 +1,7 @@
-export const regexPassword = (password: string) => {
-  const regexPassword =
-    /^(?!((?:[A-Za-z]+)|(?:[~!@#$%^&*()_+=]+)|(?:[0-9]+))$)[A-Za-z\d~!@#$%^&*()_+=]{8,16}$/;
+const regex =
+  /^(?!((?:[A-Za-z]+)|(?:[~!@#$%^&*()_+=]+)|(?:[0-9]+))$)[A-Za-z\d~!@#$%^&*()_+=]{8,16}$/;
 
-  if (!regexPassword.test(password)) {
-    return false;
-  }
-  return true;
+// 비밀번호 형식 검증 함수 (정규식)
+export const regexPassword = (password: string) => {
+  return regex.test(password);
 };
