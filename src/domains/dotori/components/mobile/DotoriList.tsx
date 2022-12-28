3abc4e141008f60dd214dbbef9f0c86fecee03d7
이지호 <cctv1998@naver.com>
2022-12-28 15:06:07 +0900

    feat: 모바일 도토리 매뉴바 기능 구현

--- src/domains/dotori/components/mobile/DotoriList.tsx
+++ src/domains/dotori/components/mobile/DotoriList.tsx
@@ -1,9 +1,20 @@
-import { MobileDotoriCard } from "domains/@shared/components";
+import { ItemId } from "@atlaskit/tree";
+import {
+  DotoriCardOptionButton,
+  MobileDotoriCard,
+} from "domains/@shared/components";
+import { useToast } from "domains/@shared/hooks";
 import { useInfiniteScroll } from "domains/@shared/hooks/useInfiniteScroll";
-import React from "react";
+import useDotoriMutation from "domains/dotori/hooks/useDotoriMutation";
+import { initialDotoriState } from "domains/dotori/utils/constants";
+import React, { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import { userSelector } from "stores/user";
 import { Dotori } from "types/dotori";
+
+export interface ActiveDotoriMenu extends Dotori {
+  isOpen: boolean;
+}
 
 interface Props {
   dotoris: Dotori[];
@@ -13,10 +24,56 @@ interface Props {
 function DotoriList({ dotoris, fetchNextPage }: Props) {
   const [subscribe] = useInfiniteScroll(fetchNextPage);
   const { name, image: profileImg } = useSelector(userSelector);
+
+  const {
+    mutateDeleteDotori,
+    mutateMoveDotori,
+    mutateRemindToggleDotori,
+    mutateClickCountDotori,
+  } = useDotoriMutation();
+
+  const { remindRecommendationToast } = useToast();
+
+  const { remindToggle } = useSelector(userSelector);
+
+  const [isActiveDotoriMenu, setIsActiveDotoriMenu] =
+    useState<ActiveDotoriMenu>({
+      ...initialDotoriState,
+      isOpen: false,
+    });
+
+  const onActiveDotoriMenu = useCallback((dotori: Dotori, isOpen: boolean) => {
+    setIsActiveDotoriMenu({ ...dotori, isOpen });
+  }, []);
+
+  const onMoveDotori = (nextFolderId: ItemId) => {
+    const requestData = {
+      bookmarkIdList: [isActiveDotoriMenu.id],
+      nextFolderId,
+    };
+    mutateMoveDotori(requestData);
+  };
+
+  const onDeleteDotori = () => mutateDeleteDotori([isActiveDotoriMenu.id]);
+
+  const onRemindToggle = (id: string, isRemind: boolean) => {
+    if (!remindToggle) {
+      remindRecommendationToast();
+      return;
+    }
+
+    const requestData = {
+      dotoriId: id,
+      remind: isRemind,
+    };
+
+    mutateRemindToggleDotori(requestData);
+  };
+
   return (
     <div>
       {dotoris.map((dotori) => {
-        const { id, link, title, description, image } = dotori;
+        const { id, link, title, description, image, remindList } = dotori;
         return (
           <MobileDotoriCard
             key={id}
@@ -24,9 +81,28 @@ function DotoriList({ dotoris, fetchNextPage }: Props) {
             title={title}
             description={description}
             imageSrc={image}
-            onClickLink={() => {}}
+            onClickLink={() => {
+              mutateClickCountDotori(id);
+            }}
             profileName={name}
             profileImageSrc={profileImg}
+            optionsButton={
+              <DotoriCardOptionButton
+                isActiveDotoriMenu={isActiveDotoriMenu}
+                isOpenDotoriMenu={
+                  isActiveDotoriMenu.id === dotori.id &&
+                  isActiveDotoriMenu.isOpen
+                }
+                isRemind={!!remindList.length}
+                link={link}
+                onActiveDotoriMenu={(isOpen: boolean) =>
+                  onActiveDotoriMenu(dotori, isOpen)
+                }
+                onDeleteDotori={onDeleteDotori}
+                onMoveDotori={onMoveDotori}
+                onRemindToggle={() => onRemindToggle(id, !!remindList.length)}
+              />
+            }
           />
         );
       })}
